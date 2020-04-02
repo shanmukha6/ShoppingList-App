23537ad20bbce37a8a7d19dce6e901dc97d01b56
Shanmukha <skondapalakala@deloitte.com>
2020-04-02 09:47:21 +0530

    Adding content to the recipes Components Outputting a List of Recipes with ngFor Displaying Recipe Details Working on the ShoppingListComponent Creating an Ingredient Model Creating and Outputting the Shopping List Adding a Shopping List Edit Section

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes=[];
+  recipes: Recipe[]=[
+  new Recipe('A test Recipe', 'This is a test', 'https://images.app.goo.gl/9SbdzeiDxvtKtxKf8' ),
+  new Recipe('A test Recipe', 'This is a test', 'https://images.app.goo.gl/9SbdzeiDxvtKtxKf8' ),
+  new Recipe('A test Recipe', 'This is a test', 'https://images.app.goo.gl/9SbdzeiDxvtKtxKf8' ),
+  new Recipe('A test Recipe', 'This is a test', 'https://images.app.goo.gl/9SbdzeiDxvtKtxKf8' )
+];
   constructor() { }
 
   ngOnInit(): void {
